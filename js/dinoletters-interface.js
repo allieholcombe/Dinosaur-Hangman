8db564664f795a88d45978c8249b1c4386f0bd58
Alexandra Holcombe and Katy Daviscourt <alexandra+katy@>
2017-03-15 14:03:09 -0700

    make alphabet clickable and have it loop through the current word array and recognize matches

--- js/dinoletters-interface.js
+++ js/dinoletters-interface.js
@@ -1,25 +1,36 @@
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var guessWord;
+var wordArray;
 // The line below is the same as $(document).ready...
 $(function(){
   for(i = 0; i < 9; i++){
-    $('#letter-box').append("<div class='letter choose-letter'>" + letters[i] + "</div>");
+    $('#letter-box').append("<div class='letter choose-letter' data-val='" + letters[i] + "'>" + letters[i] + "</div>");
   }
   for(j = 9; j < 18; j++){
-    $('#letter-box2').append("<div class='letter choose-letter'>" + letters[j] + "</div>");
+    $('#letter-box2').append("<div class='letter choose-letter' data-val='" + letters[j] + "'>" + letters[j] + "</div>");
   }
   for(k = 18; k < 26; k++){
-    $('#letter-box3').append("<div class='letter choose-letter'>" + letters[k] + "</div>");
+    $('#letter-box3').append("<div class='letter choose-letter' data-val='" + letters[k] + "'>" + letters[k] + "</div>");
   }
 
   $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1')
   .then(function(response){
-    guessWord = response[0].toString();
+    guessWord = response[0].toString().toUpperCase();
     console.log(guessWord);
-    var wordArray = guessWord.split('');
-    console.log(wordArray);
+    wordArray = guessWord.split('');
     wordArray.forEach(function(letter) {
-      $('#letter-spaces').append("<div class='letter blank " + letter + "'></div>");
+      $('#letter-spaces').append("<div class='letter blank' data-val='" + letter + "'></div>");
     })
   });
+
+  $('.choose-letter').click(function(){
+    var currentLetter = $(this).data('val');
+    console.log(currentLetter);
+    for(i = 0; i < wordArray.length; i++){
+      if(currentLetter === wordArray[i]){
+        console.log("corrent! " + i);
+      }
+    }
+  });
+
 });
